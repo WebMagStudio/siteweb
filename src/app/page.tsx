099c5d95549112ef5505d571378b0ce3ac2835c4
Magma73 <marine.magnin73@gmail.com>
2025-06-24 15:08:08 +0200

    ST10 - Afficher les filtres par catégorie et implémenter la logique du filtrage par type de service Fixes #30

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
-// import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
-// import CTAButton from "./_components/ctaButton";
 import HeroBanner from "./_components/heroBanner";
 import HeadingBlock from "./_components/headingBlock";
 import ServiceCard from "./_components/serviceCard";
 import { servicesList } from "../data/servicesList";
-// import ProjectCard from "./_components/projectCard";
-// import { projectsList } from "~/data/projectsList";
 import ProjectsGrid from "./_components/projectsGrid";
 import CTABanner from "./_components/ctaBanner";
 
@@ -64,46 +58,8 @@ export default async function Home() {
             }
           />
           <ProjectsGrid />
-          {/* <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-8 xl:gap-10">
-            {[...projectsList]
-              .sort((a, b) => b.id - a.id) // tri décroissant par id
-              .slice(0, visibleCount)
-              .map((project) => (
-                <ProjectCard key={project.id} {...project} />
-              ))}
-          </div>
-          <div className="my-10 w-full sm:max-w-3xl mx-auto lg:mx-0">
-            <button onClick={() => setVisibleCount(visibleCount + 4)} className="flex gap-2 rounded-lg border border-stroke-article bg-gradient-to-r from-nocture to-eclipse p-4 text-sm font-medium transition duration-200 hover:shadow-md hover:shadow-stroke-article">
-              <Image src="/icons/addmore.svg" width={19} height={19} alt="" />
-              Afficher plus
-            </button>
-          </div> */}
         </section>
         <section className="relative w-full">
-          {/* <div
-        className="absolute inset-0 -z-20"
-                  style={{
-          backgroundColor: "#05041f",
-          backgroundImage:
-            "linear-gradient(#ffffff 1px, transparent 1px), linear-gradient(to right, #ffffff 1px, transparent 1px)",
-          backgroundSize: "92px 92px",
-          opacity: 0.3,
-        }}
-      />
-
-      {/* Tâches lumineuses */}
-          {/* <div className="absolute top-[20%] left-[15%] w-12 h-20 bg-gradient-to-b from-white/70 to-transparent opacity-20 -z-10" />
-      <div className="absolute top-[10%] left-[50%] w-12 h-20 bg-gradient-to-b from-white/70 to-transparent opacity-20 -z-10" />
-      <div className="absolute top-[35%] left-[35%] w-12 h-20 bg-gradient-to-b from-white/70 to-transparent opacity-20 -z-10" />
-      <div className="absolute top-[50%] left-[65%] w-12 h-20 bg-gradient-to-b from-white/70 to-transparent opacity-20 -z-10" />
-      <div className="absolute top-[65%] left-[30%] w-12 h-20 bg-gradient-to-b from-white/70 to-transparent opacity-20 -z-10" /> */}
-
-          {/* <div className="w-full absolute top-130 -left-43 inset-0 -z-10 bg-[url('/img/Container.png')] bg-no-repeat bg-cover bg-center" /> */}
-
-          {/* <div
-    className="w-full aspect-[1/1] absolute -left-[40px] -top-[346px] -z-10 bg-[url('/img/Container.png')] bg-no-repeat bg-cover bg-center"
-  /> */}
-
           <div className="absolute -left-[40px] -z-10 aspect-[1/1] w-full -translate-y-1/3 bg-[url('/img/Container.png')] bg-cover bg-center bg-no-repeat" />
 
           <CTABanner
